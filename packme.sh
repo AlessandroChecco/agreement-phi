9c483523017e35bf12d53100d221b3e1de4576c2
Alessandro Checco <checcoa@tcd.ie>
2018-08-05 05:19:10 +0100

    Add files via upload

--- packme.sh
+++ packme.sh
@@ -1,2 +1,2 @@
 workon flask3
-pyinstaller --onefile --hidden-import=theano.tensor.shared_randomstreams phi.py
+pyinstaller --onefile --hidden-import=theano.tensor.shared_randomstreams --hidden-import=scipy.optimize --hidden-import scipy._lib.messagestream --hidden-import pandas._libs.tslibs.timedeltas phi.py
